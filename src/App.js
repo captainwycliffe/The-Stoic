69006c5a7ec3c935c1724cc5ba8593b75d8315c4
Wycliffe Yegon <captainwycliffe@gmail.com>
2024-04-13 23:19:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { Component } from "react";
+import "./App.css";
+import seneca from './images/seneca.jpeg';
+import marcus from './images/marcus.jpg';
+import epictitus from './images/epictitus.jpeg';
+import ryan from './images/ryan.jpeg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const imageUrls = [seneca, marcus, epictitus, ryan];
+export default class Aph4 extends Component {
+  state = { quote: "", author: "", backgroundImageIndex: 0 };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://stoic.tekloon.net/stoic-quote")
+      .then((response) => {
+        const { quote } = response.data;
+        const { author } = response.data;
+
+        this.setState({ quote, author });
+
+        this.setState((prevState) => ({
+          backgroundImageIndex:
+            (prevState.backgroundImageIndex + 1) % imageUrls.length
+        }))
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { quote, author } = this.state;
+
+    return (
+      <div
+        className="app"
+        style={{
+          backgroundImage: `url(${imageUrls[this.state.backgroundImageIndex]})`,
+          backgroundSize: "cover",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div className="quote-card">
+          <h1 className="heading">{quote}</h1>
+          <p className="author">
+            <i>~ {author} ~</i>
+          </p>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Give me advice</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
